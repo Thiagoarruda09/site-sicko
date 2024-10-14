cea08419666dce91363f0d4935dfa81accd6c9f8
Thiago Arruda <thiago.arruda.09@hotmail.com>
2024-10-14 10:05:23 -0300

    .
    .

---
+++ script.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.inicio', { duration: 1500 });
+sr.reveal('.biografia', { rotate: { x: 0, y: 80, z: 0 }, duration: 2500 });
+sr.reveal('.contatos', { duration: 1000 });
+
+window.sr=ScrollReveal({reset:true});
+    
+sr.reveal('.linha1',{duration:1000});
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.linha1', { duration: 1000 });
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.linha1', { duration: 1500 });
